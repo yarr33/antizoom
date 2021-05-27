988167e6092c2498a00f2c86b78044a8c554644e
yarr33 <57555235+yarr33@users.noreply.github.com>
2021-05-27 18:12:17 +0300

    source code

---
+++ antizoom-chrome-extension/background.js
@@ -0,0 +1,37 @@
+chrome.runtime.onInstalled.addListener(function () {
+    alert('thank you for adding this extension! this extension closes zoom pages automatically after you enter the meeting!')
+});
+
+chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+        if (tab.url.split("#")[1] == "success" && tab.title == "Launch Meeting - Zoom") {
+                chrome.tabs.remove(tab.id, function () { });
+        }
+    }
+    //alert(document.getElementsByTagName("body")[0].innerText) 
+    /*
+
+
+    "obfuscate"
+      /oo\
+     |    |
+ ^^  (vvvv)   ^^
+ \\  /\__/\  //            - yo yarr33 yo cat
+  \\/      \//               why is there an inactive line?!
+   /        \                that's inefficient fix it NOW!
+  |          |    ^  
+  /          \___/ | 
+ (            )     |
+  \----------/     /
+    //    \\_____/
+   W       W
+   
+   
+   */
+}
+);
+
+
+
+//lol nobody got the reference
